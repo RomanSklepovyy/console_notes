21b50e5a745133544de4bc2e3cb25c5c48ef3113
Roman <romansklepovyy1@gmail.com>
2020-03-24 17:30:55 +0200

    validator module

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
+const validator = require('validator');
 const getNotes = require('./notes.js');
-console.log(getNotes());
+
+console.log(validator.isURL('https://www.npmjs.com/package/validator'));
