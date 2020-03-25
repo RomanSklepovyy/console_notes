82f9826637c0b89dfcdddfff515557c7b3e26738
Roman <romansklepovyy1@gmail.com>
2020-03-25 20:22:13 +0200

    chalk module

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const validator = require('validator');
 const getNotes = require('./notes.js');
+const chalk = require('chalk');
 
-console.log(validator.isURL('https://www.npmjs.com/package/validator'));
+const greenMessage = chalk.black.bgGreen.bold;
+
+console.log(greenMessage('Success!'));
