d0a8f3c9438a5a7a2d56df6889aa8686aae1df3f
Roman <romansklepovyy1@gmail.com>
2020-03-26 13:45:00 +0200

    yargs module, implemented read, add, remove, list commands

--- index.js
+++ index.js
@@ -1,7 +1,45 @@
 const validator = require('validator');
-const getNotes = require('./notes.js');
+const yargs = require('yargs');
 const chalk = require('chalk');
+const getNotes = require('./notes.js');
 
-const greenMessage = chalk.black.bgGreen.bold;
+// Yargs version
+yargs.version('1.0.0');
 
-console.log(greenMessage('Success!'));
+// Create add command
+yargs.command({
+   command: 'add',
+   describe: 'Add a new note',
+   handler: () => {
+       console.log('Adding a new note...');
+   }
+});
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: () => {
+        console.log('Removing the note...');
+    }
+});
+
+// Create add command
+yargs.command({
+    command: 'list',
+    describe: 'Printing all notes',
+    handler: () => {
+        console.log('Printing all notes...');
+    }
+});
+
+// Create add command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: () => {
+        console.log('Reading the note...');
+    }
+});
+
+console.log(yargs.argv);
